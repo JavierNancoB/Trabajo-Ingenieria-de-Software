e9a8b0939440649af8bc56b1456f005fe395a0a1
Javier Nanco <jnanco@utem.cl>
2024-05-30 00:59:33 -0400

    se añade edit de productos

--- erp_eudora_vinos/website/urls.py
+++ erp_eudora_vinos/website/urls.py
@@ -5,4 +5,5 @@ from .models import Producto
 urlpatterns = [
     path('', views.home, name='home'),
     path('producto/', views.producto, name='producto'),
+    path('producto/edit/', views.ajuste_producto, name='ajuste_producto'),
 ]
--- erp_eudora_vinos/website/views.py
+++ erp_eudora_vinos/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Cliente
 from.models import Producto
+from .forms import ProductoForm
 
 # se crea la vista home
 
@@ -10,5 +11,15 @@ def home(request):
 def producto(request):
     productos = Producto.objects.all()
     return render(request, 'producto.html', {'productos': productos})
+
+def ajuste_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return render(request, 'productoform.html', {})
+        
+    else:
+        return render(request, 'productoform.html', {})
 
 # Create your views here.
