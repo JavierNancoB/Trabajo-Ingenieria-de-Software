62cda0cd9667305130e5e36018c73232c9174212
Leoqlia0 <lfarias@utem.cl>
2024-05-29 22:05:29 -0400

    se sube css,js,html de producto. agrega bootstrap,jquery en base . agrega carpeta js

--- erp_eudora_vinos/website/admin.py
+++ erp_eudora_vinos/website/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Cliente
-
+from.models import Producto
 admin.site.register(Cliente)
+admin.site.register(Producto)
 # Register your models here.
--- erp_eudora_vinos/website/urls.py
+++ erp_eudora_vinos/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .models import Producto
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('producto/', views.producto, name='producto'),
 ]
