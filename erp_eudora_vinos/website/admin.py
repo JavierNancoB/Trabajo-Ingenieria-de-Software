b03a85a01ab8adacd2e3642b536165d919d1342d
Leoqlia0 <lfarias@utem.cl>
2024-06-08 01:26:37 -0400

    dejé la cagá xd

--- erp_eudora_vinos/website/admin.py
+++ erp_eudora_vinos/website/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Cliente
 from.models import Producto
+from.models import Ventas
+from.models import Inventario_Y_Stock
+from.models import Alerta_stock
 # Register your models here.
 
 admin.site.register(Cliente)
 admin.site.register(Producto)
-
+admin.site.register(Ventas)
+admin.site.register(Inventario_Y_Stock)
+admin.site.register(Alerta_stock)
--- erp_eudora_vinos/website/views.py
+++ erp_eudora_vinos/website/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from.models import Producto
 from.models import Proveedores
+from.models import Alerta_stock
+from.models import Inventario_Y_Stock
+from.models import Ventas
+#from.models import Alerta_informes
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 
@@ -51,3 +55,62 @@ def delete_proveedor(request, rut_empresa):
     return redirect('/proveedor')
 
 # se crea la vista edit_producto, editamos desde>
+
+# Alertas
+@login_required
+def notificaciones(request):
+    alertas_stock = Alerta_stock.objects.all()  # Obtén todas las alertas
+    return render(request, 'notificaciones.html', {'alertas_stock': alertas_stock})
+
+"""@login_required
+def insert_alerta_stock(request):
+    if request.method == 'POST':
+        id_inventario_id = request.POST.get('id_inventario')  # Cambiado a id_inventario
+        fecha_alerta = request.POST.get('fecha_alerta')
+        if id_inventario_id and fecha_alerta:  # Validación básica
+            id_inventario = get_object_or_404(Inventario_Y_Stock, id_inventario=id_inventario_id)
+            Alerta_stock.objects.create(id_inventario=id_inventario, fecha_alerta=fecha_alerta)
+    member.save()        
+    return redirect('/notificaciones')
+    
+    """
+@login_required
+def insert_alerta_stock(request):
+    if request.method == 'POST':
+        id_inventario = request.POST.get('id_inventario')
+        fecha_alerta = request.POST.get('fecha_alerta')
+        if id_inventario and fecha_alerta :
+                alerta = Inventario_Y_Stock.objects.get(id_inventario=id_inventario)
+                member = Alerta_stock(
+                    id_inventario=alerta,
+                    fecha_alerta=fecha_alerta
+                )
+                member.save()
+                return redirect('/notificaciones')
+        
+
+
+
+
+
+@login_required
+def delete_alerta_stock(request, id_inventario):
+    alerta_stock = get_object_or_404(Alerta_stock, id_inventario=id_inventario)
+    alerta_stock.delete()
+    return redirect('notificaciones')
+
+"""@login_required
+def insert_alerta_informes(request):
+    if request.method == 'POST':
+        numero_boleta = request.POST.get('numero_boleta')
+        fecha_alerta = request.POST.get('fecha_alerta')
+        fecha_vencimiento = request.POST.get('fecha_vencimiento')
+        if numero_boleta and fecha_alerta and fecha_vencimiento:  # Validación básica
+            Alerta_informes.objects.create(numero_boleta=numero_boleta, fecha_alerta=fecha_alerta, fecha_vencimiento=fecha_vencimiento)
+    return redirect('notificaciones')
+
+@login_required
+def delete_alerta_informes(request, numero_boleta):
+    alerta_informes = get_object_or_404(Alerta_informes, numero_boleta=numero_boleta)
+    alerta_informes.delete()
+    return redirect('notificaciones')"""
